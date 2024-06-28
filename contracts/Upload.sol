46238e046cc7dc61d97105855202d763891b4995
YASH-YADAV-dynamo <yash2yk2@gmail.com>
2024-06-29 01:12:09 +0530

    fixes to git config

---
+++ contracts/Upload.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Upload {
+    struct Access {
+        address user;
+        bool access;
+    }
+
+    mapping(address => string[]) private value;
+    mapping(address => mapping(address => bool)) private ownership;
+    mapping(address => Access[]) private accessList;
+    mapping(address => mapping(address => bool)) private previousData;
+
+    function add(address _user, string calldata url) external {
+        value[_user].push(url);
+    }
+
+    function allow(address user) external {
+        ownership[msg.sender][user] = true;
+        if (previousData[msg.sender][user] == true) {
+            for (uint i = 0; i < accessList[msg.sender].length; i++) {
+                if (accessList[msg.sender][i].user == user) {
+                    accessList[msg.sender][i].access = true;
+                }
+            }
+        } else {
+            accessList[msg.sender].push(Access(user, true));
+            previousData[msg.sender][user] = true;
+        }
+    }
+
+    function disallow(address user) external {
+        ownership[msg.sender][user] = false;
+        for (uint i = 0; i < accessList[msg.sender].length; i++) {
+            if (accessList[msg.sender][i].user == user) {
+                accessList[msg.sender][i].access = false;
+            }
+        }
+    }
+
+    function display(address _user) external view returns (string[] memory) {
+        require(_user == msg.sender || ownership[_user][msg.sender], "You don't have access");
+        return value[_user];
+    }
+
+    function shareAccess() external view returns (Access[] memory) {
+        return accessList[msg.sender];
+    }
+}
---
+++ script/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Upload = await hre.ethers.getContractFactory("Upload");
+  const upload = await Upload.deploy();
+
+  await upload.deployed();
+
+  console.log("Library deployed to:", upload.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+// 0x5FbDB2315678afecb367f032d93F642f64180aa3
